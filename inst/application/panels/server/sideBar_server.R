1646af6f4ce0c719412513a2814f3cedfe0e451c
thomasdenecker <thomas.denecker@gmail.com>
2020-11-16 17:18:49 +0100

    Update sidebar

--- inst/application/panels/server/sideBar_server.R
+++ inst/application/panels/server/sideBar_server.R
@@ -2,33 +2,61 @@ observeEvent(rvEnvent$load, {
   if(rvEnvent$load){
     output$sidebar <- renderUI({
       sidebarMenu( id = "tabs",
-                   menuItem("Import", tabName = "home", icon = icon("file-import")),
-                   if( input$dataset =="input" | (input$dataset == "demo" & input$demoType == "fasta" ) ){ 
-                     menuItem("Filters",tabName = "sequenceFilters", icon = icon("filter"))
-                     },
-                   menuItem("Summary", tabName = "resume", icon = icon("file")),
-                   menuItem("PIP profile", tabName = "graphic", icon = icon("chart-area")),
-                   menuItem("Alignment", tabName = "alignmentTabItem", icon = icon("equals")),
+                   menuItem("Home", tabName = "home", icon = icon("home")),
+                   p(style = "text-align: center;margin: 0px; font-weight: bold; background-color: #3c8dbc; padding: 5px;", 'Imports'),
+                   actionButton("dataButton",
+                                "Import sequences",
+                                icon = icon("upload"),
+                                width = 200
+                   ),
+                   actionButton("annotButton",
+                                "Import annotation",
+                                icon = icon("upload"),
+                                width = 200
+                   ),
+                   if(!is.null(rvAnnotation$annotation) | input$dataset =="input" | (input$dataset == "demo" & input$demoType == "fasta" ) ){
+                     p(style = "text-align: center;margin: 0px; font-weight: bold; background-color: #3c8dbc;padding: 5px;", 'Settings')
+                   },
+
                    if( !is.null(rvAnnotation$annotation)){
                      menuItem("Annotations", tabName ="annotationTabItem", icon = icon("pen"))
                    },
+                   if( input$dataset =="input" | (input$dataset == "demo" & input$demoType == "fasta" ) ){
+                     menuItem("Filters",tabName = "sequenceFilters", icon = icon("filter"))
+                   },
+                   p(style = "text-align: center;margin: 0px; font-weight: bold; background-color: #3c8dbc;padding: 5px;", 'Results'),
+                   menuItem("Sequences information", tabName = "resume", icon = icon("file")),
+                   menuItem("PIP profile", tabName = "graphic", icon = icon("chart-area")),
+                   menuItem("Alignment", tabName = "alignmentTabItem", icon = icon("equals")),
                    downloadButton("downloadData", label = "Download", style = "width:200px;color: #444 !important; margin : 15px"),
-                   tags$hr(style=" margin : 0px 15px"), 
-                   menuItem("Session", tabName = "about", icon = icon("cubes")), 
-                   menuItem("Help", tabName = "helpPage", icon = icon("question-circle")) 
+                   p(style = "text-align: center;margin: 0px; font-weight: bold; background-color: #3c8dbc;padding: 5px;", 'More'),
+                   menuItem("Session", tabName = "about", icon = icon("cubes")),
+                   menuItem("Help", tabName = "helpPage", icon = icon("question-circle"))
       )
     })
-    
+
   } else {
     output$sidebar <- renderUI({
       sidebarMenu(id = "tabs",
-                  menuItem("Import", tabName = "home", icon = icon("file-import")),
-                  menuItem("About", tabName = "about", icon = icon("cubes")), 
+                  menuItem("Home", tabName = "home", icon = icon("home")),
+                  p(style = "text-align: center;margin: 0px; font-weight: bold; background-color: #3c8dbc;padding: 5px;", 'Imports'),
+                  actionButton("dataButton",
+                               "Import sequences",
+                               icon = icon("upload"),
+                               width = 200
+                  ),
+                  actionButton("annotButton",
+                               "Import annotation",
+                               icon = icon("upload"),
+                               width = 200
+                  ),
+                  p(style = "text-align: center;margin: 0px; font-weight: bold; background-color: #3c8dbc;padding: 5px;", 'More'),
+                  menuItem("About", tabName = "about", icon = icon("cubes")),
                   menuItem("Help", tabName = "helpPage", icon = icon("question-circle"))
       )
     })
     updateTabItems(session, "tabs", selected = "home")
     shinyjs::runjs("window.scrollTo(0, 0)")
   }
-  
+
 })
--- inst/application/panels/ui/home_ui.R
+++ inst/application/panels/ui/home_ui.R
@@ -4,23 +4,13 @@ home <- fluidPage(
   h3("PIPprofileR is a tool to easily generate Profiles of Percent Identical Positions from a fasta file (nucleotide or peptide sequences).
     With the help of an annotation file, it will be possible to simply explore the results."),
 
-  fluidRow( style="display: flex; text-align:center;",
-    column(6,style = "border : 2px #3c8dbc solid ; margin : 5px; padding : 0px 0px 20px 0px; ",
-           h2("Import your sequences", style = "margin : 0px 0px 15px 0px"),
-           helpText("Firstly, you should select a demo dataset, upload a FASTA file or a Rdata file (previous analysis)."),
-           actionButton("dataButton",
-                        "Select file",
-                        icon = icon("upload"),
-                        width = 200
-           )),
-    column(6, style = "border : 2px #3c8dbc solid ; margin : 5px; padding :0px 0px 20px 0px; ",
-           h2("Import annotation", style = "margin : 0px 0px 15px 0px"),
-           helpText("You can add an annotation file to enrich the exploration of the PIP profile or refine the search during alignments."),
-           actionButton("annotButton",
-                        "Select file",
-                        icon = icon("upload"),
-                        width = 200
-           ))
-  ),
+  h4("How to use ?"),
+  p("1- Firstly, you should select a demo dataset, upload a FASTA file or a Rdata file (previous analysis)."),
+  p("2- [not mandatory] Add an annotation file to enrich the exploration of the PIP profile or refine the search during alignments."),
+  p("3- Align sequence (if you have imported a fasta file) and explore PIP profile."),
+
   HTML('</div>')
+
+
+
 )
